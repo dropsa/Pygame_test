a31b4bff3d1492278cb7d7e67be69fad19958915
dropsa <34777333+dropsa@users.noreply.github.com>
2025-02-27 11:05:39 +0100

    Main first steps

--- pygame-particle-simulation/main.py
+++ pygame-particle-simulation/main.py
@@ -1,8 +1,23 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_WIDTH
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 display_surface = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_WIDTH))
 
 clock = pygame.time.Clock()
 
 
+def main_loop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            
+        clock.tick()
+
+        display_surface.fill((0,0,0))
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    pygame.init()
+    main_loop()
